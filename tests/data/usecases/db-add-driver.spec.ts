ffca80192a27fc7b4c960d30c78e0bd384d02bad
nunees <felipe_silva_nunes@hotmail.com>
2025-04-07 14:46:27 -0300

    test: ensure DbAddDriver calls CheckDriverByDocumentRepository with correct values

--- tests/data/usecases/db-add-driver.spec.ts
+++ tests/data/usecases/db-add-driver.spec.ts
@@ -1,18 +1,28 @@
 import { mockAddDriverParams } from '@/tests/domain/mocks/mock-driver'
 import { DbAddDriver } from '@/data/usecases/driver/db-add-driver'
-import { AddDriverRepositorySpy } from '@/tests/data/mocks/mock-db-driver'
+import {
+  AddDriverRepositorySpy,
+  CheckDriverByDocumentRepositorySpy,
+} from '@/tests/data/mocks/mock-db-driver'
 
 type SutTypes = {
   sut: DbAddDriver
   addDriverRepositorySpy: AddDriverRepositorySpy
+  checkDriverByDocumentRepositorySpy: CheckDriverByDocumentRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const addDriverRepositorySpy = new AddDriverRepositorySpy()
-  const sut = new DbAddDriver(addDriverRepositorySpy)
+  const checkDriverByDocumentRepositorySpy =
+    new CheckDriverByDocumentRepositorySpy()
+  const sut = new DbAddDriver(
+    addDriverRepositorySpy,
+    checkDriverByDocumentRepositorySpy,
+  )
   return {
     sut,
     addDriverRepositorySpy,
+    checkDriverByDocumentRepositorySpy,
   }
 }
 
@@ -44,5 +54,14 @@ describe('DbAddDriver Usecase', () => {
     const { sut } = makeSut()
     const result = await sut.add(mockAddDriverParams())
     expect(result).toBe(true)
+  })
+
+  it('Should call CheckDriverByDocumentRepository with correct values', async () => {
+    const { sut, checkDriverByDocumentRepositorySpy } = makeSut()
+    const driverParams = mockAddDriverParams()
+    await sut.add(driverParams)
+    expect(checkDriverByDocumentRepositorySpy.document).toBe(
+      driverParams.documentNumber,
+    )
   })
 })
